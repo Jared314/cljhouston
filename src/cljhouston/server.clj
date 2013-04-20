046463d62e996011df49b62e88ebe685797567f8
Jared Lobberecht <pretender314@gmail.com>
2013-04-19 22:48:24 -0500

    combining into single file and moving templates out of public folder for security

--- src/cljhouston/server.clj
+++ src/cljhouston/server.clj
@@ -1,15 +1,10 @@
 (ns cljhouston.server
-  (:require [cljhouston.views.welcome :as welcome]
-            [ring.adapter.jetty :refer :all]
+  (:require [ring.adapter.jetty :refer :all]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [compojure.handler :as handler]
-            [dieter.core :as dieter]))
-
-(defroutes site-routes
-  (GET "/" [] (welcome/render))
-  (route/resources "/")
-  (route/not-found (welcome/render)))
+            [dieter.core :as dieter]
+            [net.cgrand.enlive-html :refer :all]))
 
 (def dieter-config-options {
   :engine :v8
@@ -17,6 +12,52 @@
   :asset-roots ["resources"]
   :cache-root "resources/asset-cache"
   :cache-mode :production})
+
+(def asset-url-attr {:link :href :script :src :img :src :a :href})
+(def dieter-asset-selector (set (map #(vector (vector (first %) (attr-starts (second %) "/assets/"))) asset-url-attr)))
+(defn dieter-replace-asset-url [config]
+      (letfn [(get-asset-name [value] (.substring value 8))]
+             (fn [node]
+                 (let [attrname (-> node :tag asset-url-attr)]
+                      (assoc-in node [:attrs attrname] (dieter/link-to-asset (get-asset-name (-> node :attrs attrname)) config))))))
+
+(deftemplate base-view "base.html" [body]
+  [:#content]
+  (content body)
+  ; replace asset urls with cache-busting asset urls
+  dieter-asset-selector
+  (dieter-replace-asset-url dieter-config-options))
+
+(defsnippet index-view "index.html" [:body :> :*] [members meeting]
+  [:ul#members :li]
+  (clone-for [m members] (content m))
+  [:#meeting-date]
+  (content (first meeting))
+  [:#meeting-description]
+  (content (second meeting)))
+
+(def members
+  ["Daniel Solano Gómez"
+   "Nelson Morris"
+   "Steven Byrnes"
+   "Steven Reynolds"
+   "Jim Theriot"
+   "Robert Boone"
+   "Jeremey Barrett"])
+
+(def meetings
+  [["5/24/2012" "Coding: Work on the website."]
+   ["7/26/2012" "Clojure Macros through the Lens of Generative programming"]
+   ["8/23/2012" "Watch video from clojure/west"]
+   ["9/27/2012" "Pulling data from a database + Coding"]
+   ["10/25/2012" "How to use Enlive"]])
+
+(defn render [] (base-view (index-view members (last meetings))))
+
+(defroutes site-routes
+  (GET "/" [] (render))
+  (route/resources "/")
+  (route/not-found (render)))
 
 (def app (-> site-routes
              handler/site
